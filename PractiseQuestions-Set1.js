08196f55815d8c5d9847fadec01e1440b2695c33
Manju yadav <111909728+manjuyadav1234@users.noreply.github.com>
2024-11-24 23:11:34 +0530

    Add files via upload

---
+++ PractiseQuestions-Set1.js
@@ -0,0 +1,15 @@
+// Create a Const object called "product" to store information  related to the pen
+const product ={
+    title:"Ball Pen",
+    rating:4,
+    offer:5,
+    price:270
+};
+console.log(product);
+// Create a Const object called "profile" to store information  related to the profile
+const profile={
+    username:"Monu yadav",
+    isFollow:false,
+    followers:463,
+    following:394,
+};
