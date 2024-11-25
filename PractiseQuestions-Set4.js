284dd210e2d5bd45f396a20d8c0bcef3b5d0b9dd
Manju yadav <111909728+manjuyadav1234@users.noreply.github.com>
2024-11-25 23:14:01 +0530

    Add files via upload

---
+++ PractiseQuestions-Set4.js
@@ -0,0 +1,4 @@
+//Generate a username  based on the input. Start username with @, followed by their full name and ending with the fullname length
+let Name="Mona Yadav";
+let username="@"+Name.trim()+Name.length;
+console.log(username);
