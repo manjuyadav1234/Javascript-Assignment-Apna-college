be420adcbf16e8aebd899133c373dc434a53297d
Manju yadav <111909728+manjuyadav1234@users.noreply.github.com>
2024-11-25 09:34:39 +0530

    Add files via upload

---
+++ PractiseQuestions-Set2.js
@@ -0,0 +1,32 @@
+// WAP to check number is multiple of 5 or not
+let Number=495;
+if(Number%5==0){
+    console.log("yes");
+}
+else{
+    console.log("No");
+}
+/*WAP to give the grades to students according to their scores.
+80-100,A
+70-79,B
+60-69,C
+50-59,D
+0-45,F
+*/
+let grade=0;
+if(grade<=100 && grade>=90){
+    console.log("A");
+}
+else if(grade<=89 && grade>=70){
+    console.log("B");
+}
+else if(grade<=69 && grade>=60){
+    console.log("C");
+}
+
+else if(grade<=59 && grade>=50){
+    console.log("D");
+}
+else{
+    console.log("F")
+}
